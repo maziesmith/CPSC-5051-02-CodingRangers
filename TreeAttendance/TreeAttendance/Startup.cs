77f282f08bd6b0940f1d8715332f82c43e9cfb2b
tzhao5 <tzhaojustdoit@me.com>
2018-05-17 20:56:52 -0700

    Initial Commit

---
+++ TreeAttendance/TreeAttendance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TreeAttendance.Startup))]
+namespace TreeAttendance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
