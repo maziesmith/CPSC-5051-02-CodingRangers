c7d5bf08285387821a93d2ee5daf497c9fcea58f
Hanh Tran <hanhtran2794@gmail.com>
2018-05-20 21:41:58 -0700

    Add Student Home

---
+++ TreeAttendance/TreeAttendance/Controllers/StudentHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TreeAttendance.Controllers
+{
+    public class StudentHomeController : Controller
+    {
+        // GET: StudentHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
