67f48d6373421b5a89dfd4e8fdfbc4c429ba5113
Tianhua Zhao <tzhaojustdoit@me.com>
2018-05-22 00:23:54 -0700

    Added StudentListModel

---
+++ TreeAttendance/TreeAttendance/Models/StudentListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeAttendance.Models
+{
+    public class StudentListModel
+    {
+        public List<StudentModel> StudentList { get; set; }
+
+        public StudentListModel()
+        {
+            StudentList = new List<StudentModel>();
+        }
+    }
+}
