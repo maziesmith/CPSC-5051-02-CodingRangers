00f8ea4f5018a82dc854555fd4d06fe75df0c5f3
Tianhua Zhao <tzhaojustdoit@me.com>
2018-05-22 01:12:35 -0700

    Added SchoolDayListModel

---
+++ TreeAttendance/TreeAttendance/Models/SchoolDayListModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeAttendance.Models
+{
+    public class SchoolDayListModel
+    {
+        /// <summary>
+        /// Maintains a list of all school days so far
+        /// </summary>
+        public List<SchoolDayModel> SchoolDayList { get; set; }
+        /// <summary>
+        /// Constructor, The list is empty initially
+        /// </summary>
+        public SchoolDayListModel()
+        {
+            SchoolDayList = new List<SchoolDayModel>();
+        }
+    }
+}
