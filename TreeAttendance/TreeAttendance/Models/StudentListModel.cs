31eaaaa6b29bca934d433bbc87f7d518ce5ab552
Tianhua Zhao <tzhaojustdoit@me.com>
2018-05-22 00:25:49 -0700

    Added documentation for StudentListModel

--- TreeAttendance/TreeAttendance/Models/StudentListModel.cs
+++ TreeAttendance/TreeAttendance/Models/StudentListModel.cs
@@ -7,8 +7,13 @@ namespace TreeAttendance.Models
 {
     public class StudentListModel
     {
+        /// <summary>
+        /// Maintains a list of all current students.
+        /// </summary>
         public List<StudentModel> StudentList { get; set; }
-
+        /// <summary>
+        /// Constructor, the list is empty initially.
+        /// </summary>
         public StudentListModel()
         {
             StudentList = new List<StudentModel>();
