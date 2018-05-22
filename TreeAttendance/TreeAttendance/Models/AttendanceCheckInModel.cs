29eefdc66a466fe40522a5b58543aa5a5489eae5
Tianhua Zhao <tzhaojustdoit@me.com>
2018-05-22 00:51:24 -0700

    Added AttendanceModel and AttendanceCheckInModel

---
+++ TreeAttendance/TreeAttendance/Models/AttendanceCheckInModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TreeAttendance.Models
+{
+    /// <summary>
+    /// This class represents a check-in record of a student on a schoolday, maintains clock-in and clock-out time pair.
+    /// </summary>
+    public class AttendanceCheckInModel
+    {
+        /// <summary>
+        /// a unique id for this check-in record
+        /// </summary>
+        [Key]
+        [Display(Name = "Id", Description = "Attendance Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public String Id { get; set; }
+
+        [Display(Name = "Check-in Time", Description = "Check-in time")]
+        public DateTime ClockIn { get; set; }
+
+        [Display(Name = "Check-out Time", Description = "Check-out time")]
+        public DateTime ClockOut { get; set; }
+        /// <summary>
+        /// The Attendance model this check-in belongs to.
+        /// </summary>
+        public AttendanceModel AttendanceModel { get; set; }
+        /// <summary>
+        /// Constuctor
+        /// </summary>
+        public AttendanceCheckInModel()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ TreeAttendance/TreeAttendance/Models/AttendanceModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TreeAttendance.Models
+{
+    public class AttendanceModel
+    {
+        /// <summary>
+        /// a unique id for this attendance record
+        /// </summary>
+        [Key]
+        [Display(Name = "Id", Description = "Attendance Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+        /// <summary>
+        /// The school day of this attendance record
+        /// </summary>
+        public SchoolDayModel SchoolDay { get; set; }
+        /// <summary>
+        /// The student of the attendance record
+        /// </summary>
+        public StudentModel Student { get; set; }
+        /// <summary>
+        /// Maintains a list of AttendanceCheckInModel. If there are 3 check-ins on this day by this student, then the list size will be 3.
+        /// </summary>
+        public List<AttendanceCheckInModel> AttendanceCheckIns { get; set; }
+
+        /// <summary>
+        /// Id will be random, AttendanceChekcIns will be of size 1.
+        /// </summary>
+        public void Initialize()
+        {
+            Id = Guid.NewGuid().ToString();
+            AttendanceCheckIns = new List<AttendanceCheckInModel>();
+
+            AttendanceCheckIns.Add(new AttendanceCheckInModel());
+            AttendanceCheckIns.Last().AttendanceModel = this;
+        }
+        /// <summary>
+        /// Constuctor, Student and SchoolDay not set.
+        /// </summary>
+        public AttendanceModel()
+        {
+            Initialize();
+        }
+        /// <summary>
+        /// Set clock-in time.
+        /// </summary>
+        /// <param name="time"></param>
+        public void SetClockInTime(DateTime time)
+        {
+            AttendanceCheckIns.Last().ClockIn = time;
+        }
+        /// <summary>
+        /// Set clock-out time. After the time is set, a new AttendanceCheckInModel is created a the end of the AttendanceCheckIns list.
+        /// </summary>
+        /// <param name="time"></param>
+        public void SetClockOutTime(DateTime time)
+        {
+            AttendanceCheckIns.Last().ClockOut = time;
+            AttendanceCheckIns.Add(new AttendanceCheckInModel());
+            AttendanceCheckIns.Last().AttendanceModel = this;
+        }
+    }
+}
