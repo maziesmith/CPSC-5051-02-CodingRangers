6af38557ec7f0ef7e4df8ffe613f085d052550c7
Tianhua Zhao <tzhaojustdoit@me.com>
2018-05-27 19:25:44 -0700

    Commented AdminHomeController

--- TreeAttendance/TreeAttendance/Controllers/AdminHomeController.cs
+++ TreeAttendance/TreeAttendance/Controllers/AdminHomeController.cs
@@ -6,21 +6,37 @@ using System.Web.Mvc;
 
 namespace TreeAttendance.Controllers
 {
+    /// <summary>
+    /// Controller for the Admin section of the website
+    /// </summary>
     public class AdminHomeController : Controller
     {
-        // GET: AdminHome
+        /// <summary>
+        /// Admin dashboard, displays some quick and useful information
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// Reports Index
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ReportsIndex()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Student reports
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Reports()
         {
             return View();
         }
 
-        public ActionResult ReportsIndex()
-        {
-            return View();
-        }
+
     }
 }
