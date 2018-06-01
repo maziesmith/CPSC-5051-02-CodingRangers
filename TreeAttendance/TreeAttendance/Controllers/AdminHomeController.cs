725d6961e9426f8b057497604633cacd3fc33c63
Tianhua Zhao <tzhaojustdoit@me.com>
2018-06-01 07:15:20 -0700

    Moved reports to a separate controller

--- TreeAttendance/TreeAttendance/Controllers/AdminHomeController.cs
+++ TreeAttendance/TreeAttendance/Controllers/AdminHomeController.cs
@@ -14,37 +14,6 @@ namespace TreeAttendance.Controllers
         }
 
         /// <summary>
-        /// Generates student's attendance reports
-        /// </summary>
-        /// <returns></returns>
-        /// GET:AdminHome/Reports
-        public ActionResult Reports()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// Generates the whole class's attendance reports
-        /// </summary>
-        /// <returns></returns>
-        /// GET:AdminHome/ClassReports
-        public ActionResult ClassReports()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// Show list of students + class so user can pick for
-        /// attendance reports
-        /// </summary>
-        /// <returns></returns>
-        /// GET: AdminHome/ReportsIndex/
-        public ActionResult ReportsIndex()
-        {
-            return View();
-        }
-
-        /// <summary>
         /// Admin Login
         /// </summary>
         /// <returns></returns>
--- TreeAttendance/TreeAttendance/Controllers/ReportsController.cs
+++ TreeAttendance/TreeAttendance/Controllers/ReportsController.cs
@@ -8,8 +8,32 @@ namespace TreeAttendance.Controllers
 {
     public class ReportsController : Controller
     {
-        // GET: AdminHome
+        /// <summary>
+        /// Show list of students + class so user can pick for
+        /// attendance reports
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Generates student's attendance reports
+        /// </summary>
+        /// <returns></returns>
+        /// GET:AdminHome/Reports
+        public ActionResult StudentReports()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Generates the whole class's attendance reports
+        /// </summary>
+        /// <returns></returns>
+        /// GET:AdminHome/ClassReports
+        public ActionResult ClassReports()
         {
             return View();
         }
