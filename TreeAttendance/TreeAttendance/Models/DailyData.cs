0df05e81f6ff39808ef5c5ea85266eebffc0a91f
Tianhua Zhao <tzhaojustdoit@me.com>
2018-06-02 08:47:58 -0700

    Added Models for student reports

---
+++ TreeAttendance/TreeAttendance/Models/DailyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeAttendance.Models
+{
+    public class DailyData
+    {
+        public string Date { get; set; }
+        public double HoursAttended { get; set; }
+        public double HoursExpected { get; set; }
+        public double AccumulativeHoursAttended { get; set; }
+        public double AccumulativeHoursExpected { get; set; }
+    }
+}
---
+++ TreeAttendance/TreeAttendance/Models/StudentReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeAttendance.Models
+{
+    public class StudentReport
+    {
+        public string Month { get; set; }
+        public string StudentName { get; set; }
+        public List<DailyData> data { get; set; }
+        public int DaysPresent { get; set; }
+        public int DaysAbsentExcused { get; set; }
+        public int DaysAbsentUnexcused { get; set; }
+        public int HoursAttended { get; set; }
+        public int HoursMissing { get; set; }
+        public int DaysOnTime { get; set; }
+        public int DaysLate { get; set; }
+        public int DaysStayed { get; set; }
+        public int DaysLeftEarly { get; set; }
+    }
+}
