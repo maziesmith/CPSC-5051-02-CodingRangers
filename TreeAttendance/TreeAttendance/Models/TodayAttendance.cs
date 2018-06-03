c6829434ca14d869701710f650894087d621ca22
Tianhua Zhao <tzhaojustdoit@me.com>
2018-06-03 05:29:37 -0700

    Implemented today's info board on admin dashboard

---
+++ TreeAttendance/TreeAttendance/Models/TodayAttendance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeAttendance.Models
+{
+    public class TodayAttendance
+    {
+        public List<AttendanceModel> Attendance { get; set; }
+        public int Current { get; set; }
+        public int Present { get; set; }
+        public int OnTime { get; set; }
+        public int Left { get; set; }
+        public TodayAttendance(List<AttendanceModel> attendance)
+        {
+            Attendance = attendance;
+            Compute();
+        }
+
+        public void Compute()
+        {
+            Current = Attendance.Count;
+            Present = 0;
+            OnTime = 0;
+            Left = 0;
+            if(Current != 0)
+            {
+                foreach (var item in Attendance)
+                {
+                    if(item.Status != Enums.AttendanceStatusEnum.AbsentExcused && item.Status != Enums.AttendanceStatusEnum.AbsentUnexcused)
+                    {
+                        Present++;
+                    }
+                    if(item.Status == Enums.AttendanceStatusEnum.OnTime || item.Status == Enums.AttendanceStatusEnum.OnTimeLeft)
+                    {
+                        OnTime++;
+                    }
+                    if(item.Status == Enums.AttendanceStatusEnum.LateLeft || item.Status == Enums.AttendanceStatusEnum.OnTimeLeft)
+                    {
+                        Left++;
+                    }
+                }
+            }
+        }
+    }
+}
